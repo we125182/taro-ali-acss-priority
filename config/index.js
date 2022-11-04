58d6455522787d138a7dcf5e2b62247607b0728c
Jiande <we1215107007@163.com>
2022-11-04 21:49:17 +0800

    feat: 开启esbuild压缩

--- config/index.js
+++ config/index.js
@@ -24,8 +24,10 @@ const config = {
   framework: 'vue',
   compiler: 'webpack5',
   cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
+  jsMinimizer: 'esbuild',
+  cssMinimizer: 'parcelCss',
   mini: {
     postcss: {
       pxtransform: {
