c19e0fb54774a7fcee0d3a0bb14556e7c015bec3
Jiande <we1215107007@163.com>
2022-11-04 16:55:45 +0800

    init: 新增taro alipay样式优先级bugdemo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text class="text-color">{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg: '红色'
+    }
+  }
+}
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="index">
+    <hello-world />
+  </view>
+</template>
+
+<script>
+import HelloWorld from '../../components/HelloWorld.vue'
+export default {
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
