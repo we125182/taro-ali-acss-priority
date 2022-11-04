1bfbd10db1aa935eb9db64394fc82afb57f7ef6e
Jiande <we1215107007@163.com>
2022-11-04 21:46:46 +0800

    chore: 优化ali-acss-plugin

--- config/ali-acss-plugin.js
+++ config/ali-acss-plugin.js
@@ -1,23 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+import { sources } from 'webpack';
 
-export default (ctx, options) => {
-  const { outputPath } = ctx.paths
-  const getFilePath = (filename) => path.join(outputPath, filename)
-  ctx.onBuildComplete(() => {
-    try {
-      fs.accessSync(getFilePath('common.acss'), fs.constants.F_OK)
-    } catch (error) {
-      ctx.writeFileToDist({
-        filePath: 'common.acss',
-        content: ''
-      })
+const { RawSource } = sources;
+const COMMON_STYLE = 'common.acss';
+const APP_STYLE = 'app.acss';
+
+export default (ctx) => {
+
+  /**
+   *
+   */
+  ctx.modifyBuildAssets(({ assets }) => {
+    /**
+     * 新增app.acss用以导入全局及公共样式
+     */
+    if (!assets[APP_STYLE]) {
+      assets[APP_STYLE] = new RawSource(`@import './global.acss';\n@import './common.acss';\n`);
     }
-    ctx.writeFileToDist({
-      filePath: 'app.acss',
-      content: `@import './global.acss';@import './common.acss';`
-    })
+    /**
+     * 防止common.acss为空的情况
+     */
+    if (!assets[COMMON_STYLE]) {
+      assets[COMMON_STYLE] = new RawSource('');
+    }
   })
+
+  /**
+   * 修改miniCssExtractPlugin配置:将app.acss改名为global.scss
+   */
   ctx.modifyWebpackChain(({ chain }) => {
     chain.plugin('miniCssExtractPlugin').tap(args => {
       args[0].filename = function(pathData) {
